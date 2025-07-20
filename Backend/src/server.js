59d59351531b0c0b15ef9587186b5d5f6f514120
Muhammed Fais <pmuhammedfais2001@gmail.com>
2025-07-20 13:28:46 -1000

    c

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -19,7 +19,7 @@ app.use(
     credentials: true,
     origin: [
       "http://localhost:5173",
-      // 🔁 replace with real frontend URL
+      "https://your-frontend-name.vercel.app", // 🔁 replace with real frontend URL
     ],
   })
 );
